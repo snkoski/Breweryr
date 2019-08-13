390999123bb89df453badaa2b7c541360edac33d
snkoski <shawnnkoski@gmail.com>
2019-08-13 10:59:25 -0400

    Make Error404 component and add to routes if url is not found

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
-// import BreweryDetails from './components/Brewerys/BreweryDetails';
 import Details from './components/Details';
 import Header from './components/Header';
+import Error404 from './components/Error404';
 import './App.css';
 
 
@@ -14,8 +14,8 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
-          {/* <Route path="/brewery/:id" component={BreweryDetails} /> */}
           <Route path="/brewery/:id" component={Details} />
+          <Route component={Error404} />
         </Switch>
       </BrowserRouter>
     </div>
